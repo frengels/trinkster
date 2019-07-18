74abdb96dbcff6217ea9b239b303f824376ea488
Frederik Engels <frederik.engels92@gmail.com>
2019-07-19 00:16:16 +0200

    Add tests for intrusive list

--- shell.nix
+++ shell.nix
@@ -5,5 +5,5 @@ let
 in
 stdenv.mkDerivation {
   name = "trinkster-dev";
-  buildInputs = [ meson ninja pkgconfig gdb udev x11 pixman libxkbcommon libGL wlroots wayland wayland-protocols glm clang_8 gcc9 callable_traits ];
+  buildInputs = [ meson ninja pkgconfig gdb udev x11 pixman libxkbcommon libGL wlroots wayland wayland-protocols glm clang_8 gcc9 catch2 callable_traits ];
 }
